fe3ce76967a59bceb2a39bff9decfb2d8fa524cb
anson <anson@ansondeMac-mini.local>
2021-06-21 14:35:43 +0800

    update

--- boot/dog.sh
+++ boot/dog.sh
@@ -106,8 +106,8 @@ function PathInitSlaves(){
             ssh-keyscan -H $ip >> ~/.ssh/known_hosts
             echo "ansible-playbook create user admin on this $ip"
             ansible-playbook $BOOT_TASKS_PATH/createuser.yml -i $ip, -e "user_add=$USER ansible_user=$USER_INIT ansible_ssh_pass=$PASSWD_INIT ansible_become_pass=$PASSWD_INIT condition=false"
-            #echo "ansible-playbook mount disk on this $ip"
-            #ansible-playbook $BOOT_TASKS_PATH/diskpart.yml  -i $ip, -e "user_add=$USER  disk=$DISK diskfullpath=$DISKFULLPATH" --private-key=/home/admin/.ssh/$PRIVATEKEY
+            echo "ansible-playbook mount disk on this $ip"
+            ansible-playbook $BOOT_TASKS_PATH/diskpart.yml  -i $ip, -e "user_add=$USER  disk=$DISK diskfullpath=$DISKFULLPATH" --private-key=/home/admin/.ssh/$PRIVATEKEY
             echo "ansible-playbook init kubernetes slave path on this $ip"
             ansible-playbook $BOOT_TASKS_PATH/bootstrap.yml -i $ip, -e "hostname=$hname" --private-key=/home/admin/.ssh/$PRIVATEKEY
             echo "ansible-playbook install docker $DOCKER_VERSION"
