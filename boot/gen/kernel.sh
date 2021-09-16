e1d2cb5b94b2eeb78ed4c1fba42d9d4620b81a79
anson <anson@ansondeMac-mini.local>
2021-09-16 15:14:36 +0800

    updte

--- boot/gen/kernel.sh
+++ boot/gen/kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum -y install wget
-wget https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-5.4.144-1.el7.elrepo.x86_64.rpm -P /opt/kubernetes/
+#yum -y install wget
+#wget https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-5.4.144-1.el7.elrepo.x86_64.rpm -P /opt/kubernetes/
 rpm -ivh /opt/kubernetes/kernel-lt-5.4.144-1.el7.elrepo.x86_64.rpm
 A=`sudo awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg |grep "(5.4.144-1.el7.elrepo.x86_64) 7 (Core)" |awk '{print $1}'`
 grub2-set-default $A
